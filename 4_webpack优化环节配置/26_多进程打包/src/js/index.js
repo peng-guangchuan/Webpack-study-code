7a7e281d3cfa3be5d6215c878b3cd3f3714b136c
peng-guangchuan <pgc2018@126.com>
2021-11-26 14:50:16 +0800

    webpack中使用多线程打包

---
+++ 4_webpack优化环节配置/26_多进程打包/src/js/index.js
@@ -0,0 +1,24 @@
+import { mul } from './test';
+import '../css/index.css';
+
+function sum(...args) {
+  return args.reduce((p, c) => p + c, 0);
+}
+
+// eslint-disable-next-line
+console.log(mul(2, 3));
+// eslint-disable-next-line
+console.log(sum(1, 2, 3, 4));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(() => {
+        console.log('sw注册成功了~');
+      })
+      .catch(() => {
+        console.log('sw注册失败了~');
+      });
+  });
+}
